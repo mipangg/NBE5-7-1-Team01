b1300a2b11f09d7428fbdd8bd1231cb2018b2517
mipangg <morganjeong119@gmail.com>
2025-04-29 12:03:38 +0900

    feat(ItemImages, Items, ItemsCategory, OrderItems, Orders): 엔티티 생성

---
+++ src/main/java/io/binary/coffeenotfound_404/domain/Items.java
@@ -0,0 +1,59 @@
+package io.binary.coffeenotfound_404.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Entity
+@Table(name = "items")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Items {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Column(nullable = false)
+    private String name;
+
+    @Setter
+    @Column(nullable = false)
+    private Integer price;
+
+    @Setter
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Setter
+    @Column(nullable = false)
+    private ItemsCategory Category;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "item_images_id", nullable = false)
+    private ItemImages images;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Builder
+    public Items(String name, Integer price, Integer stock, ItemsCategory category, ItemImages images) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        Category = category;
+        this.images = images;
+    }
+}
---
+++ src/main/java/io/binary/coffeenotfound_404/domain/OrderItems.java
@@ -0,0 +1,47 @@
+package io.binary.coffeenotfound_404.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "order_items")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderItems {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "orders_id", nullable = false)
+    private Orders orders;
+
+    @ManyToOne
+    @JoinColumn(name = "items_id", nullable = false)
+    private Items items;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    public Integer getItemTotalPrice() {
+        return items.getPrice() * quantity;
+    }
+
+    @Builder
+    public OrderItems(Orders orders, Items items, Integer quantity) {
+        this.orders = orders;
+        this.items = items;
+        this.quantity = quantity;
+    }
+}
