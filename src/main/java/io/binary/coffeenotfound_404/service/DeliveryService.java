1ece4227d47609a6c2045be8777e07bc21be761e
mipangg <morganjeong119@gmail.com>
2025-05-02 20:50:20 +0900

    feat(admin): 관리자 배달 시키기 기능 추가, 배달 완료된 주문 내역 삭제 기능 추가

--- src/main/java/io/binary/coffeenotfound_404/service/DeliveryService.java
+++ src/main/java/io/binary/coffeenotfound_404/service/DeliveryService.java
@@ -3,11 +3,10 @@ package io.binary.coffeenotfound_404.service;
 import io.binary.coffeenotfound_404.dao.OrdersRepository;
 import io.binary.coffeenotfound_404.domain.Orders;
 import jakarta.transaction.Transactional;
-import java.time.LocalTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -17,33 +16,16 @@ import org.springframework.stereotype.Service;
 public class DeliveryService {
 
     private final OrdersRepository ordersRepository;
-    private final LocalTime DELIVERY_START_TIME = LocalTime.of(14, 0, 0);
-    private final LocalTime DELIVERY_END_TIME = LocalTime.of(14, 1, 0);
 
-    // 1 현재가 오후 2시 인지 판별
-    // 2 오후 2시인 경우, 주문 리스트들을 배송 완료 처리
-    // 매 분마다 실행되도록 설정 (실제로는 1분 단위로 점검)
-    @Scheduled(cron = "0 * * * * *") // 초 분 시 일 월 요일 (매분 0초에 실행)
-    public void deliver() {
-        LocalTime now = LocalTime.now();
+    public Orders updateByEmailandOrderId(String email, Long orderId) {
+        List<Orders> ordersList = ordersRepository.findListByEmail(email);
 
-        if (now.isAfter(DELIVERY_START_TIME) && now.isBefore(DELIVERY_END_TIME)) {
-            log.info(goDelivery() + "개 배송 완료");
-        }
-    }
-
-    public Integer goDelivery() {
-        Integer countOfDeliveredOrders = 0;
-
-        List<Orders> ordersList = ordersRepository.findAll();
         for (Orders order : ordersList) {
-            if (!order.isDelivered()) {
+            if (order.getId().equals(orderId)) {
                 order.setDelivered(true);
+                return order;
             }
-            countOfDeliveredOrders++;
         }
-
-        return countOfDeliveredOrders;
+        throw new NoSuchElementException("입력된 email와 ID의 주문이 존재하지 않습니다.");
     }
-
 }
